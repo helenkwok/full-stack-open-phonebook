4c3f6cec48cb570cc189d140cc1b8be2855d9687
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-10-12 11:14:41 +1030

    add e2e tests

--- index.js
+++ index.js
@@ -113,10 +113,8 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT
-  app.listen(PORT)
-  console.log(`Server running on port ${PORT}`)
-}
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
 
 module.exports = app
